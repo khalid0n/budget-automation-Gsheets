724477b7f557b8e4f0a11fc6d194529e1a9dc6b5
Khalid Al-Sahli <alsahli.khaled.n@gmail.com>
2020-09-22 16:50:57 +0300

    update README

---
+++ README.md
@@ -0,0 +1,31 @@
+# Budget Automation with Google Sheets Template
+Automated Monthly Budget Tracking (expenses, income) with Google Sheet template.
+the idea is the following:
+1. read from your bank statement using [pandas](https://pandas.pydata.org/), fetch transaction values
+2. populate these info to a monthly budget sheet om said values.
+
+### Why ?
+suppose you want to track your monthly budget. you would -probably- manually log every transaction which can be a tedious task.
+
+
+### Budget Template Overview
+you can find Monthly budget template in [spreadsheet template gallery](https://docs.google.com/spreadsheets/u/0/?ftv=1&folder=0ACoSgW1iveL-Uk9PVA).
+
+here's a short [YouTube video](https://youtu.be/yfGmQl0Sn4c?t=30) to explain how template works
+
+As of now, Automation Processing will be done in **Transactions** sheet
+
+
+
+## Steps
+- Create your Google Spreadsheet Template, add your *sheet Id* from url to `config.json` 
+- `pip install -r requirements.txt` or `pip3 install -r requirements.txt`
+- extract your bank statement *in xls format to same dir*. name should be "bank_statement.xls"
+#### Auth for Google Spreadsheet
+- Create a project in [Google Developer Console](https://console.developers.google.com/) & add [Google Sheets API](https://developers.google.com/sheets/api)
+- create service account key from *credentials*
+- Role should be **Project > Editor**
+- save the resulting private key as JSON. name it "client_secret.json"
+
+
+
--- budget.py
+++ budget.py
@@ -1,6 +1,6 @@
 import os
 import bank_statement_parser
-
+import json
 from apiclient import discovery
 from google.oauth2 import service_account
 
@@ -9,10 +9,12 @@ try:
               "https://www.googleapis.com/auth/drive.file"]
     secret_file = os.path.join(os.getcwd(), 'client_secret.json')
 
-    spreadsheet_id = '1vP1Pqq89b6Mjgsg7sPi_9rSa0H8Yn3XNhyRFwTFj1Ig'
-    summary_sheet_expenses_planned = 'Summary!D28:D40'
-    expenses_area = 'Transactions!B5:E30'
-    income_area = 'Transactions!G5:J30'
+    with open('config.json') as config_file:
+        json_data = json.load(config_file)
+        spreadsheet_id = json_data["sheetId"]
+
+    expenses_cell_range = 'Transactions!B5:E1000'
+    income_cell_range = 'Transactions!G5:J1000'
 
     credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
     service = discovery.build('sheets', 'v4', credentials=credentials)
@@ -29,10 +31,10 @@ try:
     }
 
     # Expenses
-    service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, body=expenses_data, range=expenses_area,
+    service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, body=expenses_data, range=expenses_cell_range,
                                            valueInputOption='USER_ENTERED').execute()
     # Income
-    service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, body=income_data, range=income_area,
+    service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, body=income_data, range=income_cell_range,
                                            valueInputOption='USER_ENTERED').execute()
 
 except OSError as e:
