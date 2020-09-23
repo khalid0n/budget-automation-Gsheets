dfc4c1df241bce14779e5c24a339c16a87f7ee1e
Khalid Al-Sahli <alsahli.khaled.n@gmail.com>
2020-09-23 17:40:42 +0300

    adding demo

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import re
 # from YYYY-MM-DD  to   M/D/YYYY
 def parse_transaction_date(date):
     try:
-        new_date = datetime.datetime.strptime(str(date), '%Y-%m-%d')
+        new_date = datetime.datetime.strptime(date, '%Y-%m-%d')
         new_date = '{0}/{1}/{2}'.format(new_date.month, new_date.day, new_date.year)
         return new_date
     except ValueError:
